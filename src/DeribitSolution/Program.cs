d1ea8856085d3917674612996b996c2a0eea7251
gsi-emiliogonzalez <124933006+gsi-emiliogonzalez@users.noreply.github.com>
2023-02-10 13:30:02 -0500

    feat(SettingUp):creation of the project scaffoling (#23)
    * feat(SettingUp):creation of the project scaffoling
    
    including libraries for dependency injection, console logger, spectre console, configuration
    
    closes #1
    
    * fix(SettingUp): fixing project folder routing
    
    close #1

---
+++ src/DeribitSolution/Program.cs
@@ -0,0 +1,39 @@
+﻿using ConsoleApp.TestClass;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Security.Authentication.ExtendedProtection;
+
+var serviceCollection = new ServiceCollection();
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder
+        .AddFilter("Microsoft", LogLevel.Warning)
+        .AddFilter("System", LogLevel.Warning)
+        .AddFilter("NonHostConsoleApp.Program", LogLevel.Debug)
+        .AddConsole();
+});
+ILogger logger = loggerFactory.CreateLogger<Program>();
+logger.LogInformation("Created log feature");
+Console.WriteLine();
+IConfiguration configuration;
+var path = Directory.GetCurrentDirectory();
+configuration = new ConfigurationBuilder()
+    .SetBasePath(path)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+logger.LogInformation("Injected appsettings.json file to project");
+Console.WriteLine();
+serviceCollection.AddSingleton<IConfiguration>(configuration);
+logger.LogInformation("Injected configuration");
+Console.WriteLine();
+serviceCollection.AddSingleton<TestClass>();
+var serviceProvider = serviceCollection.BuildServiceProvider();
+logger.LogInformation("Created Service provider");
+Console.WriteLine();
+logger.LogInformation("Getting object to test dependency injection");
+Console.WriteLine();
+var test = serviceProvider.GetService<TestClass>();
+test.TestMethod();
+logger.LogInformation("Depdency injection configured succesfully");
+Console.WriteLine();
---
+++ src/DeribitSolution/TestClass/TestClass.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.TestClass
+{
+    public class TestClass
+    {
+        private readonly IConfiguration configuration;
+
+        public TestClass(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public void TestMethod()
+        {
+            var dataFromJsonFile = configuration.GetSection("Name").Value;
+            Console.WriteLine(dataFromJsonFile);
+        }
+
+    }
+}
