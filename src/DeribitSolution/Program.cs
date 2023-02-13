b254e90f777066d1b727aca2551edef290009ad7
Yoan Carlos Machado Espinosa <yoan@factory.generalsoftwareinc.com>
2023-02-13 10:03:36 -0500

    feat(Console App): add style and definitions
    - implement Spectre abstraction to infrastructure to use dependency injection
    - add command to show output in logs or Spectre Console
    - update dependency injection to
    use host builder
    - add launch profiles
    
    closes #15

--- src/DeribitSolution/Program.cs
+++ src/DeribitSolution/Program.cs
@@ -1,39 +1,35 @@
-﻿using ConsoleApp.TestClass;
-using Microsoft.Extensions.Configuration;
+﻿using ConsoleApp.Commands;
+using ConsoleApp.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System.Security.Authentication.ExtendedProtection;
+using ServiceClient;
+using Spectre.Console.Cli;
 
-var serviceCollection = new ServiceCollection();
-using var loggerFactory = LoggerFactory.Create(builder =>
-{
-    builder
-        .AddFilter("Microsoft", LogLevel.Warning)
-        .AddFilter("System", LogLevel.Warning)
-        .AddFilter("NonHostConsoleApp.Program", LogLevel.Debug)
-        .AddConsole();
-});
-ILogger logger = loggerFactory.CreateLogger<Program>();
-logger.LogInformation("Created log feature");
-Console.WriteLine();
-IConfiguration configuration;
-var path = Directory.GetCurrentDirectory();
-configuration = new ConfigurationBuilder()
-    .SetBasePath(path)
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+var host = Host.CreateDefaultBuilder()
+    .UseConsoleLifetime()
+    .ConfigureLogging((_, logging) =>
+    {
+        logging.ClearProviders();
+        logging.AddSimpleConsole(options => options.SingleLine= true);
+    })
+    .ConfigureServices(services => {
+        services.AddLogging();
+        services.AddServiceClient();
+        services.AddTransient<AsyncLogCommand>();
+        services.AddTransient<AsyncSpectreCommand>();
+        services.AddSingleton<ITypeRegistrar>(new TypeRegistrar(services));
+    })
     .Build();
-logger.LogInformation("Injected appsettings.json file to project");
-Console.WriteLine();
-serviceCollection.AddSingleton<IConfiguration>(configuration);
-logger.LogInformation("Injected configuration");
-Console.WriteLine();
-serviceCollection.AddSingleton<TestClass>();
-var serviceProvider = serviceCollection.BuildServiceProvider();
-logger.LogInformation("Created Service provider");
-Console.WriteLine();
-logger.LogInformation("Getting object to test dependency injection");
-Console.WriteLine();
-var test = serviceProvider.GetService<TestClass>();
-test.TestMethod();
-logger.LogInformation("Depdency injection configured succesfully");
-Console.WriteLine();
+
+var app = new CommandApp(host.Services.GetRequiredService<ITypeRegistrar>());
+
+app.Configure(config => {
+    config.AddBranch("output", c =>
+    {
+        c.AddCommand<AsyncLogCommand>("log");
+        c.AddCommand<AsyncSpectreCommand>("spectre");
+    });
+});
+
+await app.RunAsync(args);
