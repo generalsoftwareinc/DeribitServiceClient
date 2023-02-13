993a832869a513dfb52440d90d186379835b4853
Yoan Carlos Machado Espinosa <yoan@factory.generalsoftwareinc.com>
2023-02-13 10:03:36 -0500

    feat(Console App): add style and definitions
    - implement Spectre abstraction to infrastructure to use dependency injection
    - add command to show output in logs or Spectre Console
    - update dependency injection to
    use host builder
    - add launch profiles
    
    closes #15

---
+++ src/DeribitSolution/Commands/AsyncSpectreCommand.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Logging;
+using Spectre.Console.Cli;
+using Spectre.Console;
+
+namespace ConsoleApp.Commands;
+
+internal class AsyncSpectreCommand : AsyncCommand<AsyncSpectreCommand.Settings>
+{
+    public override async Task<int> ExecuteAsync(CommandContext context, Settings settings)
+    {
+        var table = new Table().Centered();
+        await AnsiConsole.Live(table)
+            .StartAsync(async ctx =>
+            {
+                var num = 1;
+                table.AddColumn("Number");
+                table.AddColumn("Name");
+                table.AddColumn("Last Name");
+                ctx.Refresh();
+                await Task.Delay(1000);
+                while (true)
+                {
+                    table.Rows.Clear();
+                    for (var i = 0; i < settings.RowsCount; i++)
+                    {
+                        table.AddRow(new Markup($"[bold]{num++}[/]"), new Markup("[green]Test[/]"), new Markup("[red]Test[/]"));
+                        ctx.Refresh();
+                    }
+                    await Task.Delay(1000);
+                }
+            });
+        return 0;
+    }
+
+    public sealed class Settings : CommandSettings
+    {
+        [CommandOption ("-s|--show")]
+        public int? RowsCount { get; set; } = 10;
+    }
+}
---
+++ src/ServiceClient/ServiceCollection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ServiceClient
+{
+    public static class ServiceCollection
+    {
+        public static void AddServiceClient(this IServiceCollection services)
+        {
+            // TODO add services here
+        }
+    }
+}
