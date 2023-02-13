2c67b41490652d5ecad493a1664d1f80a66753ea
gsi-juandavid <juan.david@factory.generalsoftwareinc.com>
2023-02-13 19:24:10 +0200

    ft-7: Add secure configuration to API Secret and API Key

--- src/DeribitSolution/Program.cs
+++ src/DeribitSolution/Program.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using System.Security.Authentication.ExtendedProtection;
 
 var serviceCollection = new ServiceCollection();
 using var loggerFactory = LoggerFactory.Create(builder =>
@@ -21,19 +20,25 @@ var path = Directory.GetCurrentDirectory();
 configuration = new ConfigurationBuilder()
     .SetBasePath(path)
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddUserSecrets("c252577f-cf0f-45f3-99b3-4243ba11d930")
     .Build();
 logger.LogInformation("Injected appsettings.json file to project");
+logger.LogInformation("Injected secured settings to project");
 Console.WriteLine();
 serviceCollection.AddSingleton<IConfiguration>(configuration);
 logger.LogInformation("Injected configuration");
 Console.WriteLine();
 serviceCollection.AddSingleton<TestClass>();
+var apiKey = configuration.GetSection("DeribitAPI").Get<ServiceClient.ApiKey>();
+if (apiKey != null)
+    serviceCollection.AddSingleton(apiKey);
 var serviceProvider = serviceCollection.BuildServiceProvider();
 logger.LogInformation("Created Service provider");
 Console.WriteLine();
 logger.LogInformation("Getting object to test dependency injection");
 Console.WriteLine();
 var test = serviceProvider.GetService<TestClass>();
-test.TestMethod();
+test?.TestMethod();
+var apiKey2 = serviceProvider.GetService<ServiceClient.ApiKey>();
 logger.LogInformation("Depdency injection configured succesfully");
 Console.WriteLine();
---
+++ src/ServiceClient/ApiKey.cs
@@ -0,0 +1,12 @@
+﻿namespace ServiceClient
+{
+    /// <summary>
+    /// Deribit API access key
+    /// </summary>
+    public class ApiKey
+    {
+        public string Key { get; set; }
+        public string Secret { get; set; }
+
+    }
+}
