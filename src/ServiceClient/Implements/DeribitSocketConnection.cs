475bd9932da2e8274938417c8019b6bb25db06a0
Yoan Carlos Machado Espinosa <yoan@factory.generalsoftwareinc.com>
2023-02-15 14:12:55 -0500

    fix: issue detected in main that break in execution time
    relate to: #37

--- src/ServiceClient/Implements/DeribitSocketConnection.cs
+++ src/ServiceClient/Implements/DeribitSocketConnection.cs
@@ -1,63 +1,58 @@
-﻿using ServiceClient.Abstractions;
+﻿using Microsoft.Extensions.Options;
+using ServiceClient.Abstractions;
 using ServiceClient.DTOs;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.WebSockets;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ServiceClient.Implements
+namespace ServiceClient.Implements;
+
+public class DeribitSocketConnection : IDeribitSocketConnection
 {
-    public class DeribitSocketConnection : IDeribitSocketConnection
+    private readonly WebSocketOptions options;
+
+    private ClientWebSocket? socket;
+
+    public DeribitSocketConnection(IOptions<WebSocketOptions> options) 
+    { 
+        this.options = options.Value;
+    }
+
+    public ClientWebSocket ClientWebSocket
     {
-        private readonly WebSocketOptions options;
-
-        private ClientWebSocket? socket;
-
-        public DeribitSocketConnection(WebSocketOptions options) 
-        { 
-            this.options = options;
-        }
-
-        public ClientWebSocket ClientWebSocket
+        get
         {
-            get
-            {
-                if (socket != null)
-                    return socket;
-                socket = new ClientWebSocket
-                {
-                    Options =
-                    {
-                        KeepAliveInterval = TimeSpan.FromSeconds(options.KeepAliveIntervalInSeconds)
-                    }
-                };
+            if (socket != null)
                 return socket;
-            }
-        }
-
-        public async Task SocketConnectAsync(CancellationToken cancellationToken)
-        {
-            var url = new Uri(options.Url);
-
-            await ClientWebSocket
-                .ConnectAsync(url, cancellationToken)
-                .Timeout(options.ConnectionTimeoutInMilliseconds);
-        }
-
-        public async Task SocketDisconnectAsync(CancellationToken cancellationToken)
-        {
-            await ClientWebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Close", cancellationToken);
+            socket = new ClientWebSocket
+            {
+                Options =
+                {
+                    KeepAliveInterval = TimeSpan.FromSeconds(options.KeepAliveIntervalInSeconds)
+                }
+            };
+            return socket;
         }
     }
 
-    public static class TaskExtensions
+    public async Task SocketConnectAsync(CancellationToken cancellationToken)
     {
-        public static async Task Timeout(this Task task, int timeoutInMilliseconds)
-        {
-            if (await Task.WhenAny(task, Task.Delay(timeoutInMilliseconds)) != task)
-                throw new TimeoutException($"timed out after {timeoutInMilliseconds} milliseconds");
-        }
+        var url = new Uri(options.Url);
+
+        await ClientWebSocket
+            .ConnectAsync(url, cancellationToken)
+            .Timeout(options.ConnectionTimeoutInMilliseconds);
+    }
+
+    public async Task SocketDisconnectAsync(CancellationToken cancellationToken)
+    {
+        await ClientWebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Close", cancellationToken);
+    }
+}
+
+public static class TaskExtensions
+{
+    public static async Task Timeout(this Task task, int timeoutInMilliseconds)
+    {
+        if (await Task.WhenAny(task, Task.Delay(timeoutInMilliseconds)) != task)
+            throw new TimeoutException($"timed out after {timeoutInMilliseconds} milliseconds");
     }
 }
