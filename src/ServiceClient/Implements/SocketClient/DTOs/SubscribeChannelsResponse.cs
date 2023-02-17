0e49a3362a172a30b1c0e2830f11f2eb399d5bf1
gsi-robertogarcia <roberto.garcia@factory.generalsoftwareinc.com>
2023-02-17 13:25:43 -0500

    solved suggestions

--- src/ServiceClient/Implements/SocketClient/DTOs/SubscribeChannelsResponse.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace ServiceClient.Implements.SocketClient.DTOs;
-
-class SubscribeChannelsResponse : Response
-{
-    [JsonPropertyName("result")]
-    public string[] Result { get; set; }
-}
--- src/ServiceClient/Implements/SocketClient/DeribitSocketClient.cs
+++ src/ServiceClient/Implements/SocketClient/DeribitSocketClient.cs
@@ -169,14 +169,13 @@ internal class DeribitSocketClient : IDeribitClient
         };
 
         await SendAsync("private/subscribe", data, token);
-        var jsonResult = await ReadStringAsync(token);
-        var subscriptionResult = JsonSerializer.Deserialize<SubscribeChannelsResponse>(jsonResult);
+        var channelsSubscribed = await ReadAsync<string[]>(token);
 
-        if (subscriptionResult == null)
+        if (channelsSubscribed == null || channelsSubscribed.Result == null)
             throw new Exception("Can't subscribe to the channels");
 
         var notSubscribedChannels = data.channels
-            .Except(subscriptionResult.Result)
+            .Except(channelsSubscribed.Result)
             .ToArray();
 
         if (notSubscribedChannels.Any())
