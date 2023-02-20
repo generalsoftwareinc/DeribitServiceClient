87307049a4533582f3e17710e20e71646c00ce31
Yoan Carlos Machado Espinosa <yoan@factory.generalsoftwareinc.com>
2023-02-20 10:20:44 -0500

    refactor:  improve the option validations and dependency injection
    related to #14

--- src/ServiceClient/Implements/SocketClient/DTOs/DeribitOptions.cs
+++ src/ServiceClient/Implements/SocketClient/DTOs/DeribitOptions.cs
@@ -1,14 +1,28 @@
-﻿namespace ServiceClient.Implements.SocketClient.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServiceClient.Implements.SocketClient.DTOs;
 
 public sealed class DeribitOptions
 {
+    [Required]
     public string ClientId { get; set; } = string.Empty;
+    [Required]
     public string ClientSecret { get; set; } = string.Empty;
+    [Required]
     public string WebSocketUrl { get; set; } = string.Empty;
+    [Required]
+    [Range(1, 5000)]
     public int ConnectionTimeoutInMilliseconds { get; set; }
+    [Required]
+    [Range(1, 100)]
     public int KeepAliveIntervalInSeconds { get; set; }
+    [Required]
     public string InstrumentName { get; set; } = string.Empty;
+    [Required]
     public string TickerInterval { get; set; } = string.Empty;
+    [Required]
     public string BookInterval { get; set; } = string.Empty;
+    [Required]
+    [Range(1, 3000)]
     public int HeartBeatInterval { get; set; }
 }
