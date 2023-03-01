71777e84de77430e9dc7502648069afc66f34b67
Tamás Bilik <tbilik@vertigotech.eu>
2023-03-01 10:10:55 +0100

    Folder and project renamings

---
+++ src/Deribit.ApiClient.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Deribit.ApiClient.Tests.Unit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ src/Deribit.ApiClient/Abstractions/IDeribitSocketClient.cs
@@ -0,0 +1,15 @@
+﻿using Deribit.ApiClient.DTOs;
+
+namespace Deribit.ApiClient.Abstractions
+{
+    internal interface IDeribitClient
+    {
+        event EventHandler<BookReadedEventArgs>? OnBookReaded;
+        event EventHandler<TickerReadedEventArgs>? OnTickerReaded;
+        Task CheckAvailabilityAsync(CancellationToken token);
+        Task InitializeAsync(CancellationToken token);
+        Task SubscribeAsync(CancellationToken token);
+        Task ContinueReadAsync(CancellationToken token);
+        Task DisconnectAsync(CancellationToken token); 
+    }
+}
