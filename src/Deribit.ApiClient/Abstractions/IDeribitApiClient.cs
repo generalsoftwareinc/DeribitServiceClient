77a73d1c91fc29d921efd914cf65c17955772b45
Tamás Bilik <tbilik@vertigotech.eu>
2023-03-01 10:10:55 +0100

    Folder and project renamings

--- src/Deribit.ApiClient/Abstractions/IDeribitApiClient.cs
+++ src/Deribit.ApiClient/Abstractions/IDeribitApiClient.cs
@@ -4,6 +4,9 @@ public interface IDeribitApiClient
 {
     long TickerMessagesCount { get; }
     long BookMessagesCount { get; }
+    long SubscriptionMessagesCount { get; }
+    long HeartBeatMessagesCount { get; }
+    long TokenRefreshMessagesCount { get; }
 
     Task RunAsync(CancellationToken cancellationToken);
     Task DisconnectAsync(CancellationToken cancellationToken);
